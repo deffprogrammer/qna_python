cb7aa57e52e2445742e735a8b03d54933b4a2ceb
Deff_ <96669422+deffprogrammer@users.noreply.github.com>
2021-12-27 06:44:11 +0700

    Add files via upload
    first add

---
+++ questions_and_answer.py
@@ -0,0 +1,44 @@
+print("Welcome in game Questions and Answer!!")
+
+name = input("What's your name : ")
+ready = input("Ok {} do you ready play this game (yes/no) : ".format(name))
+
+questions = 4
+score = 0
+
+if ready.lower() == "yes":
+    ans = input("What animal do you like? : ")
+    if ans.lower() == "cat":
+        print("Correct!")
+        score += 1
+    else:
+        print("Incorrect!")
+
+    ans = input("What sport do you like? : ")
+    if ans.lower() == "football":
+        print("Correct!")
+        score += 1
+    else:
+        print("Incorrect")
+
+    ans = input("What food do you like? : ")
+    if ans.lower() == "fried rice":
+        print("Correct!")
+        score += 1
+    else:
+        print("Incorrect!")
+
+    ans = input("What kind of place do you like? : ")
+    if ans.lower() == "panorama":
+        print("Correct!")
+        score += 1
+    else:
+        print("Incorrect")
+
+else:
+    print("Ok, thank you ", name, "\n")
+
+total = (score/questions) * 100
+
+print("Your score is : {:.1f}%".format(total))
+print("This end, Goodbye...")
